523f9c144078496a74e1ce0c57372fc85593af9b
Jiyooung <jiyoung.simm@gmail.com>
2021-01-20 17:03:06 +0900

    solved by JY

---
+++ jungol/JY_J1810.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JY_J1810 {
+    static int pick[];
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        // System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        print(arr, arr.length);
+        
+        for (int j = 0; j < 7; j++) {
+            System.out.println(arr[pick[j]]);
+        }
+    }
+
+    public static void print(int[] arr, int n) {
+        pick = new int[7];
+        for (int i = 0; i < (1 << n); i++) {
+            if (count(i) == 7) { // 조합 중 요소가 7개인 것만 출력
+                int sum = 0;
+                int p = 0;
+                for (int j = 0; j < n; j++) {
+                    if ((i & (1 << j)) != 0) {  // 7개 요소 더하기
+                        sum += arr[j];
+                        pick[p++] = j;
+                    }
+                }
+                if (sum == 100) {
+                    return;
+                }
+            }
+        }
+    }
+
+    // num을 2진수로 표현했을때 1의 갯수 리턴
+    public static int count(int num) {
+        int count = 0;
+
+        while (num > 0) {
+            if ((num & 1) == 1)
+                count++;
+            num >>= 1;
+        }
+
+        return count;
+    }
+}
---
+++ jungol/READMD.md
@@ -0,0 +1,7 @@
+# [jungol](http://www.jungol.co.kr/)
+
+제출 시 class 이름을 Main으로 하고 제출<br>
+아래 문장은 주석처리 후 제출
+```java
+System.setIn(new FileInputStream("input.txt"));
+```
