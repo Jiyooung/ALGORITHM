e2fa45ab22dcf4c311ca3281bc5acb9a23284ff4
Jiyooung <jiyoung.simm@gmail.com>
2021-01-27 17:26:04 +0900

    solved by JY

---
+++ baekjoon/2021년/2021-01/JY_B10998_2.js
@@ -0,0 +1,8 @@
+/*
+baekjoon 10998 : A×B
+solved by JY
+DATE : 2020.01.20
+*/
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin", "utf8").trim().split(" ");
+console.log(input[0] * input[1]);
