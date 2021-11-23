96e576419cb8485e28d9e5d46c46dcda4536a361
Jiyooung <jiyoung.simm@gmail.com>
2021-11-23 14:02:03 +0900

    solved by JY

---
+++ baekjoon/2021년/2021-11/JY_B1292.py
@@ -0,0 +1,12 @@
+# baekjoon 1292 : 쉽게 푸는 문제
+# solved by JY
+# DATE : 2021.11.21
+# 구현
+
+from sys import stdin
+input = stdin.readline
+start, end = map(int, input().split())
+num = []
+for i in range(46):
+	num += [i] * i
+print(sum(num[start-1:end]))
